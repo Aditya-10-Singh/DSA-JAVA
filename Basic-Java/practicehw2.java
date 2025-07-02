bced53977ac9e178c1a550a9188d7fbef4dfea94
Aditya <adityasingh00552@gmail.com>
2025-07-02 11:05:57 +0530

    basic java topics

--- Basic-Java/practicehw2.java
+++ Basic-Java/practicehw2.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class practicehw2 {
     public static void main(String[] args) {
         //make a program that takes the radius of a circle as input, calculates its radius and area and prints it as output to the user
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        System.out.println(radius);
+        System.out.println(3.14 * radius);
     }
     
 }
