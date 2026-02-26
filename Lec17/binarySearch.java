b450048cb5093f9099a89c79d8cb9ad094161fe1
Aditya <adityasingh00552@gmail.com>
2026-02-26 21:27:10 +0530

    Binary Search

---
+++ Lec17/binarySearch.java
@@ -0,0 +1,37 @@
+package Lec17;
+
+public class binarySearch {
+    
+    public static int binarysearch(int[] arr, int target) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start <= end) {
+            
+            int mid = start + (end - start) / 2;
+            
+            if(arr[mid] == target) {
+                return mid;
+            }
+            else if(arr[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {1,3,5,7,9,11,15};
+        int target = 7;
+        
+        int result = binarysearch(arr, target);
+        
+        System.out.println(result);
+    }
+}
